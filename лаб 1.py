a51a0763d4e3c8ca396627110a3afb2a4e2e8e4d
Богдан <bogdanganzara@gmail.com>
2025-09-10 16:25:43 +0300

    Add files via upload

---
+++ лаб 1.py
@@ -0,0 +1,16 @@
+
+a = float(input("Введіть a = "))
+b = float(input("Введіть b = "))
+
+if a < 0 or b < 0:
+    print("Помилка: числа a та b повинні бути додатними.")
+    exit()
+
+if a > b:
+    X = a / b - 1
+elif a == b:
+    X = -25
+else:
+    X = (a**3 - 5) / a
+
+print(f"Результат: X =", X)
